0ba5cba4325bfb4acdb55ded7891489d7a967a82
Erik Riquelme <erikone2311@gmail.com>
2024-11-27 09:47:13 +0100

    Add guard clause for root element

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./Components/App/App";
+import "@fontsource/poppins/index.css";
+import "./styles/styles.css";
 
-createRoot(document.querySelector(".root")!).render(
+const root = document.querySelector(".root");
+
+if (!root) {
+  throw new Error("Missing root element");
+}
+
+createRoot(root!).render(
   <StrictMode>
     <App />
   </StrictMode>
