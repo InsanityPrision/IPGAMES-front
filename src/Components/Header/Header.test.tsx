49fffc3f49fb065427624ccbbf90ff31c3ebf0d6
Erik Riquelme <erikone2311@gmail.com>
2024-11-27 15:34:56 +0100

    Add test for Header component when rendered

---
+++ src/Components/Header/Header.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+describe("Given the Header component", () => {
+  describe("When it rendered", () => {
+    test("Then it should show 'IPGAMES' in ha heading", () => {
+      render(<Header />);
+
+      const headerTitle = screen.getByRole("heading", {
+        name: /ipgames/i,
+      });
+
+      expect(headerTitle).toBeInTheDocument();
+    });
+
+    test("Then it should show the IPGAMES icon with alternative text 'IPGAMES icon'", () => {
+      render(<Header />);
+
+      const brandIcon = screen.getByAltText(/ipgames icon/i);
+
+      expect(brandIcon).toBeInTheDocument();
+    });
+  });
+});
