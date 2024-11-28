a36190f66e1cf67b36e05977bd44afcdc3973a38
Erik Riquelme <erikone2311@gmail.com>
2024-11-28 16:17:52 +0100

    Create GameListPage page component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 
 const App: React.FC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 };
 
 export default App;
---
+++ src/games/pages/GameListPage/GameListPage.tsx
@@ -0,0 +1,9 @@
+const GameListPage: React.FC = () => {
+  return (
+    <main className="main-container">
+      <h1>Games</h1>
+    </main>
+  );
+};
+
+export default GameListPage;
