ca485fe085b74cba94656ecf9be5374c16edda22
Erik Riquelme <erikone2311@gmail.com>
2024-11-28 23:49:40 +0100

    Add pseudoclass for a line in active link

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -5,7 +5,6 @@ const NavMenu: React.FC = () => {
   return (
     <nav className="navMenu-container">
       <NavLink to={"/games"} className={"navMenu-container__link"}>
-        <div className="navMenu-container__link--active"></div>
         <img src="/home.svg" alt="home icon" width={20} height={20} />
         <span className="navMenu-container__text">Home</span>
       </NavLink>
