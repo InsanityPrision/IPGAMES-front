357ecc869f779d08ea3f552bc26170fa4bc316e5
Erik Riquelme <erikone2311@gmail.com>
2024-11-28 16:17:18 +0100

    Create browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App/App";
 import "@fontsource/poppins/index.css";
 import "./styles/styles.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 const root = document.querySelector(".root");
 
@@ -12,6 +13,6 @@ if (!root) {
 
 createRoot(root).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
