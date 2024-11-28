f33ea495682d6b8f5525352f63bf8ae8bd65cefc
Erik Riquelme <erikone2311@gmail.com>
2024-11-28 11:40:55 +0100

    Change h1 to a span for headercomponent

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,8 +4,8 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="brand-container">
-        <img src="/brand.svg" alt="IPGAMES icon" width={32} height={32} />
-        <h1 className="header__title">IPGAMES</h1>
+        <img src="/brand.svg" alt="IPGAMES logo" width={32} height={32} />
+        <span className="header__title">IPGAMES</span>
       </div>
     </header>
   );
