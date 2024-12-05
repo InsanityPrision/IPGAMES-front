ac3f276071d3cdadcbf18426d4c53fbad0233b7f
Erik Riquelme <erikone2311@gmail.com>
2024-12-05 19:11:21 +0100

    Create GameCard component

---
+++ src/game/components/GameCard/GameCard.tsx
@@ -0,0 +1,42 @@
+import { Game } from "../../types";
+import "./GameCard.css";
+
+interface GameCardProps {
+  game: Game;
+}
+
+const GameCard: React.FC<GameCardProps> = ({ game }) => {
+  const starsCount: number[] = Array(game.rate).fill(0);
+
+  return (
+    <article className="game-card">
+      <img
+        className="game-card__image"
+        src={game.imageUrl}
+        alt={game.imageAlt}
+        width={250}
+        height={135}
+      />
+      <div>
+        <div className="game-card__header">
+          <h2 className="game-card__name">{game.name}</h2>
+          <ol className="game-card__raiting">
+            {starsCount.map((_starCount, index) => {
+              return (
+                <li key={index}>
+                  <img src="/star.svg" alt={`Rated level ${index + 1}`} />
+                </li>
+              );
+            })}
+          </ol>
+        </div>
+        <div className="game-card__price">
+          <span>Price:</span>
+          <span className="game-card__price-number">{game.price}</span>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default GameCard;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { StrictMode } from "react";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
 import { createRoot } from "react-dom/client";
 import "@fontsource/poppins/index.css";
@@ -7,7 +6,6 @@ import "@fontsource/poppins/700.css";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/300.css";
 import AppRouter from "./router";
-import { store } from "./game/store";
 import "./styles/styles.css";
 
 const root = document.querySelector(".root");
@@ -18,10 +16,8 @@ if (!root) {
 
 createRoot(root).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </StrictMode>,
 );
