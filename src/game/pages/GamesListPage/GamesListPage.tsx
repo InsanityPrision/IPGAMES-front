8f5f254062def098ef0e50ca5144543706fb9e1d
Erik Riquelme <erikone2311@gmail.com>
2024-12-08 22:22:01 +0100

    Add feedback in GamesListPage loading games

--- src/game/pages/GamesListPage/GamesListPage.tsx
+++ src/game/pages/GamesListPage/GamesListPage.tsx
@@ -1,18 +1,48 @@
 import { useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
 import GamesCardsList from "../../components/GamesCardsList/GamesCardsList";
 import useGames from "../../hooks/useGames";
+import "react-toastify/dist/ReactToastify.css";
+import "./GamesListPage.css";
 
 const GameListPage: React.FC = () => {
   const { games, loadGames } = useGames();
 
   useEffect(() => {
     loadGames();
+
+    toast.promise(
+      loadGames,
+      {
+        pending: {
+          render() {
+            return "Loading games...";
+          },
+          icon: () => <span className="loader"></span>,
+        },
+        error: {
+          render() {
+            return "Failed loading games";
+          },
+          icon: () => <img src="/error.svg" alt="" />,
+        },
+      },
+      {
+        closeButton: false,
+      },
+    );
   }, [loadGames]);
 
   return (
     <>
       <h1 className="page-title">Games</h1>
       <GamesCardsList games={games} />
+      <ToastContainer
+        position="top-center"
+        hideProgressBar={true}
+        autoClose={false}
+        icon={false}
+      />
     </>
   );
 };
