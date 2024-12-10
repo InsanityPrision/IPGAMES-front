57c79fae6c3a4345248c0e5957f87d7f11195d5e
Erik Riquelme <erikone2311@gmail.com>
2024-12-10 10:13:49 +0100

    Delete div brand container inside the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,8 @@ import "./Header.css";
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div className="brand-container">
-        <img src="/brand.svg" alt="IPGAMES logo" width={32} height={32} />
-        <span className="header__title">IPGAMES</span>
-      </div>
+      <img src="/brand.svg" alt="IPGAMES logo" width={32} height={32} />
+      <span className="header__title">IPGAMES</span>
     </header>
   );
 };
