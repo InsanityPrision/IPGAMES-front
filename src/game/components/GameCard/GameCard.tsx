cca23cfe615fecc4e6cdba4fa3ef4aba57a04eb6
Erik Riquelme <erikone2311@gmail.com>
2024-12-10 15:56:42 +0100

    Add depeding if the game is free show free or game price

--- src/game/components/GameCard/GameCard.tsx
+++ src/game/components/GameCard/GameCard.tsx
@@ -6,7 +6,17 @@ interface GameCardProps {
 }
 
 const GameCard: React.FC<GameCardProps> = ({ game }) => {
-  const starsCount: number[] = Array(game.rate).fill(0);
+  const starsNumbers: number[] = Array(game.rate).fill(0);
+
+  const renderPrice = () => {
+    return (
+      <span
+        className={game.isFree ? "game-card__price-number--free" : undefined}
+      >
+        {game.isFree ? "FREE" : `${game.price}$`}
+      </span>
+    );
+  };
 
   return (
     <article className="game-card">
@@ -21,9 +31,9 @@ const GameCard: React.FC<GameCardProps> = ({ game }) => {
         <div className="game-card__header">
           <h2 className="game-card__name">{game.name}</h2>
           <ol className="game-card__raiting">
-            {starsCount.map((_starCount, index) => {
+            {starsNumbers.map((_starsNumber, index) => {
               return (
-                <li key={index}>
+                <li key={starsNumbers.length - index}>
                   <img
                     src="/star.svg"
                     alt={`Rated level ${index + 1}`}
@@ -36,8 +46,8 @@ const GameCard: React.FC<GameCardProps> = ({ game }) => {
           </ol>
         </div>
         <div className="game-card__price">
-          <span>Price:</span>
-          <span className="game-card__price-number">{game.price}</span>
+          <span className="game-card__price-title">Price:</span>
+          {renderPrice()}
         </div>
       </div>
     </article>
