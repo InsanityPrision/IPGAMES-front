9f23939deac22030b0e173bc8c5ca3ce32e55b27
Erik Riquelme <erikone2311@gmail.com>
2024-12-10 10:14:20 +0100

    Rename navMenu classes

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -4,9 +4,9 @@ import "./NavMenu.css";
 const NavMenu: React.FC = () => {
   return (
     <nav className="nav-menu">
-      <NavLink to="/games" className="navMenu-container__link">
+      <NavLink to="/games" className="nav-menu-container__link">
         <img src="/home.svg" alt="Go to home" width={20} height={20} />
-        <span className="navMenu-container__text">Home</span>
+        <span className="nav-menu-container__text">Home</span>
       </NavLink>
     </nav>
   );
